2bff42faecc699d6dc4b4d2d1d885fac13b9e71b
Kaushik Kumar Sarma <kaushikkumarsarma046@gmail.com>
2024-07-12 15:45:10 +0530

    added ayush dutta

--- AI_legal_advisor/app.py
+++ AI_legal_advisor/app.py
@@ -1,130 +1,97 @@
+import os
+import time
+import streamlit as st
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain_together import Together
-import os
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import ConversationalRetrievalChain
-import streamlit as st
-import time
-import streamlit as st
-from langchain.memory import ConversationBufferWindowMemory
-try:
-    if "memory" not in st.session_state:
-        st.session_state.memory = ConversationBufferWindowMemory(k=2, memory_key="chat_history", return_messages=True)
-except Exception as e:
-    st.error(f"Error initializing session state: {e}")
-    st.stop()
-
-# Continue with the rest of your Streamlit app
 
 st.set_page_config(page_title="LawGPT")
-col1, col2, col3 = st.columns([1,4,1])
-with col2:
-    st.image("https://github.com/harshitv804/LawGPT/assets/100853494/ecff5d3c-f105-4ba2-a93a-500282f0bf00")
 
-st.markdown(
-    """
-    <style>
-div.stButton > button:first-child {
-    background-color: #ffd0d0;
-}
-div.stButton > button:active {
-    background-color: #ff6262;
-}
-   div[data-testid="stStatusWidget"] div button {
-        display: none;
-        }
-    
-    .reportview-container {
-            margin-top: -2em;
-        }
-        #MainMenu {visibility: hidden;}
-        .stDeployButton {display:none;}
-        footer {visibility: hidden;}
-        #stDecoration {display:none;}
-    button[title="View fullscreen"]{
-    visibility: hidden;}
-        </style>
-""",
-    unsafe_allow_html=True,
-)
-
-def reset_conversation():
-  st.session_state.messages = []
-  st.session_state.memory.clear()
-
+# Initialize session state
+if "show_chat" not in st.session_state:
+    st.session_state.show_chat = False
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
 if "memory" not in st.session_state:
-    st.session_state.memory = ConversationBufferWindowMemory(k=2, memory_key="chat_history",return_messages=True) 
+    st.session_state.memory = ConversationBufferWindowMemory(k=2, memory_key="chat_history", return_messages=True)
 
-embeddings = HuggingFaceEmbeddings(model_name="nomic-ai/nomic-embed-text-v1",model_kwargs={"trust_remote_code":True,"revision":"289f532e14dbbbd5a04753fa58739e9ba766f3c7"})
-# db = FAISS.load_local("ipc_vector_db", embeddings)
-db = FAISS.load_local("ipc_vector_db", embeddings, allow_dangerous_deserialization=True)
+# Function to initialize the chatbot components
+def initialize_chatbot():
+    embeddings = HuggingFaceEmbeddings(model_name="nomic-ai/nomic-embed-text-v1", model_kwargs={"trust_remote_code": True, "revision": "289f532e14dbbbd5a04753fa58739e9ba766f3c7"})
+    db = FAISS.load_local("ipc_vector_db", embeddings, allow_dangerous_deserialization=True)
+    db_retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 4})
 
-db_retriever = db.as_retriever(search_type="similarity",search_kwargs={"k": 4})
+    prompt_template = """<s>[INST]This is a chat template and As a legal chat bot specializing in Indian Penal Code queries, your primary objective is to provide accurate and concise information based on the user's questions. 
+    Also mention the act numbers according to IPC and previous judgement on similar sitations if available .Do not generate your own questions and answers. You will adhere strictly to the instructions provided, offering relevant context from the knowledge base while avoiding unnecessary details. Your responses will be brief, to the point, and in compliance with the established format. If a question falls outside the given context, you will refrain from utilizing the chat history and instead rely on your own knowledge base to generate an appropriate response. You will prioritize the user's query and refrain from posing additional questions. The aim is to deliver professional, precise, and contextually relevant information pertaining to the Indian Penal Code.
+    CONTEXT: {context}
+    CHAT HISTORY: {chat_history}
+    QUESTION: {question}
+    ANSWER:
+    </s>[INST]
+    """
+    prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question', 'chat_history'])
 
-prompt_template = """<s>[INST]This is a chat template and As a legal chat bot specializing in Indian Penal Code queries, your primary objective is to provide accurate and concise information based on the user's questions. 
-Also mention the act numbers according to IPC and previous judgement on similar sitations if available .Do not generate your own questions and answers. You will adhere strictly to the instructions provided, offering relevant context from the knowledge base while avoiding unnecessary details. Your responses will be brief, to the point, and in compliance with the established format. If a question falls outside the given context, you will refrain from utilizing the chat history and instead rely on your own knowledge base to generate an appropriate response. You will prioritize the user's query and refrain from posing additional questions. The aim is to deliver professional, precise, and contextually relevant information pertaining to the Indian Penal Code.
-CONTEXT: {context}
-CHAT HISTORY: {chat_history}
-QUESTION: {question}
-ANSWER:
-</s>[INST]
-"""
+    llm = Together(
+        model="mistralai/Mistral-7B-Instruct-v0.2",
+        temperature=0.5,
+        max_tokens=1024,
+        together_api_key="829b64678275cfdd5fbc043ea2fe54ff6404aa82381b3b11ef69617c91f132d5"
+    )
 
-prompt = PromptTemplate(template=prompt_template,
-                        input_variables=['context', 'question', 'chat_history'])
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        memory=st.session_state.memory,
+        retriever=db_retriever,
+        combine_docs_chain_kwargs={'prompt': prompt}
+    )
 
-# You can also use other LLMs options from https://python.langchain.com/docs/integrations/llms. Here I have used TogetherAI API
+    return qa
 
-# TOGETHER_AI_API= os.environ['TOGETHER_AI']
-# llm = Together(
-#     model="mistralai/Mistral-7B-Instruct-v0.2",
-#     temperature=0.5,
-#     max_tokens=1024,
-#     together_api_key=f"{TOGETHER_AI_API}"
-# )
-# TOGETHER_AI_API= os.environ['TOGETHER_AI']
-llm = Together(
-    model="mistralai/Mistral-7B-Instruct-v0.2",
-    temperature=0.5,
-    max_tokens=1024,
-    together_api_key=f"829b64678275cfdd5fbc043ea2fe54ff6404aa82381b3b11ef69617c91f132d5"
-)
-qa = ConversationalRetrievalChain.from_llm(
-    llm=llm,
-    memory=st.session_state.memory,
-    retriever=db_retriever,
-    combine_docs_chain_kwargs={'prompt': prompt}
-)
+# Function to reset the conversation
+def reset_conversation():
+    st.session_state.messages = []
+    st.session_state.memory.clear()
 
-for message in st.session_state.messages:
-    with st.chat_message(message.get("role")):
-        st.write(message.get("content"))
+# Function to show the initial description page
+def show_description():
+    st.title("AI Legal Assistant - LawGPT")
+    st.image("AILA.jpg")
+    st.markdown("""
+        Welcome to LawGPT, an AI-powered legal assistant specializing in Indian Penal Code queries. 
+        This tool provides accurate and concise information based on your questions, including act numbers according to IPC 
+        and previous judgments on similar situations if available.
+    """)
+    st.button("Try Now", on_click=lambda: st.session_state.update({"show_chat": True}))
 
-input_prompt = st.chat_input("Say something")
+# Show the description or chatbot based on session state
+if not st.session_state.show_chat:
+    show_description()
+else:
+    qa = initialize_chatbot()
+    for message in st.session_state.messages:
+        with st.chat_message(message.get("role")):
+            st.write(message.get("content"))
 
-if input_prompt:
-    with st.chat_message("user"):
-        st.write(input_prompt)
+    input_prompt = st.chat_input("Say something")
 
-    st.session_state.messages.append({"role":"user","content":input_prompt})
+    if input_prompt:
+        with st.chat_message("user"):
+            st.write(input_prompt)
 
-    with st.chat_message("assistant"):
-        with st.status("Thinking 💡...",expanded=True):
-            result = qa.invoke(input=input_prompt)
+        st.session_state.messages.append({"role": "user", "content": input_prompt})
 
-            message_placeholder = st.empty()
+        with st.chat_message("assistant"):
+            with st.status("Thinking 💡...", expanded=True):
+                result = qa.invoke(input=input_prompt)
+                message_placeholder = st.empty()
+                full_response = "⚠️ **_Note: Information provided may be inaccurate._** \n\n\n"
+                for chunk in result["answer"]:
+                    full_response += chunk
+                    time.sleep(0.02)
+                    message_placeholder.markdown(full_response + " ▌")
+                st.button('Reset All Chat 🗑️', on_click=reset_conversation)
 
-            full_response = "⚠️ **_Note: Information provided may be inaccurate._** \n\n\n"
-        for chunk in result["answer"]:
-            full_response+=chunk
-            time.sleep(0.02)
-            
-            message_placeholder.markdown(full_response+" ▌")
-        st.button('Reset All Chat 🗑️', on_click=reset_conversation)
-
-    st.session_state.messages.append({"role":"assistant","content":result["answer"]})
+        st.session_state.messages.append({"role": "assistant", "content": result["answer"]})
